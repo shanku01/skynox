fa848fe24d95fef3a6c1ca89cd1737ebd4b7ff83
Shashank Pradhan <shashankpradhan911@gmail.com>
2020-11-05 05:42:36 +0530

    checking heroku

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const path =require('path');
-const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'public','index.html'))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
