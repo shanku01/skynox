d1c9758028a2e78ec1c546ee38ab4dff70db7bf1
Shashank Pradhan <shashankpradhan911@gmail.com>
2020-11-05 05:34:11 +0530

    checking heroku

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const app = express();
 const path =require('path');
 const port = process.env.port || 3000;
 
-app.listen(port, () => {
-    console.log(`Our app is running on port ${ port }`);
-})
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'public','index.html'))
 })
